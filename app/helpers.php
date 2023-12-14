220733aa4be24130ef75750a093b03cfebb89915
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-12-14 18:53:02 +0200

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -23,3 +23,32 @@ function build_url(array $parts): string
         (isset($parts['query']) ? "?{$parts['query']}" : '') .
         (isset($parts['fragment']) ? "#{$parts['fragment']}" : '');
 }
+
+// Workarounds
+function current_locale_code(): string
+{
+    return mb_strtoupper(mb_substr(get_locale(), 0, 2));
+}
+
+function languages_list(): array
+{
+    $languages = apply_filters('wpml_active_languages', null, [
+        'skip_missing' => 0,
+        'orderby' => 'order',
+        'order' => 'desc',
+        ]);
+
+    return collect($languages)
+        ->map(function ($language) {
+            $item = new \stdClass;
+            $item->active = $language['active'];
+            $item->activeAncestor = null;
+            $item->title = $language['native_name'];
+            $item->url = $language['url'];
+            $item->label = sprintf(__('%s', 'helsinki-testbed'), $language['native_name']);
+            $item->disabled = $language['missing'];
+            $item->children = false;
+            return $item;
+        })
+        ->toArray();
+}
